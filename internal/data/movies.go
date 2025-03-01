ee089bc02cf9b37c9351ba6f173da281f0e9383e
Sanjay Bhattacahrya <sanjbh@gmail.com>
2025-03-01 17:55:11 +0530

    commit

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -93,7 +93,16 @@ func (m MovieModel) Update(movie *Movie) error {
 		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
 		WHERE id = $5
 		RETURNING version`
-	return nil
+
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error {
